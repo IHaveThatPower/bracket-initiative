b0d40395f5b4d5024fa500beeb200aaba0e39667
Ryan McClure <mcc@mcc3d.com>
2023-08-01 16:02:30 -0400

    Big refactor, removing leftover SFI stuff and improving functionality
    * Removed remaining elements from SFI module (from which this
      originated)
    * Refactored initiative handling callbacks with a specific focus on
      ensuring the "follower" functionality works as intended, which is
      now the primary (sole?) purpose of the socket.
    * Refactored diagnostic messages so that they are gated behind a CONFIG
      property, which is setup in the init hook.

--- esmodules/bracket.class.js
+++ esmodules/bracket.class.js
@@ -3,253 +3,41 @@ export class BracketInitiative
 	static MODULE_NAME = 'bracket-initiative';
 	static SOCKET_NAME = 'module.bracket-initiative';
 
-	/*
-	static FLAG_ACTION_CHOSEN = 'action-chosen';
-	static FLAG_ACTION_TARGET = 'action-target';
-	static FLAG_LAST_ACTION = 'last-action';
-
-	static FLAG_BONUS_ACTION_CHOSEN = 'bonus-action-chosen';
-	static FLAG_BONUS_ACTION_TARGET = 'bonus-action-target';
-	static FLAG_LAST_BONUS_ACTION = 'last-bonus-action';
-	*/
-
 	static FLAG_ROLL_RESULT = 'roll-result';
+
+	/**
+	 * Helper for printing diagnostic messages, if enabled
+	 *
+	 * @param	mixed
+	 * @return	void
+	 */
+	static debug()
+	{
+		const self = BracketInitiative;
+		if (CONFIG.debug.BracketInitiative)
+			console.log("BracketInitiative |", arguments);
+	}
 
 	/**
 	 * Execute socket calls or respond to them
 	 *
-	 * @param	string settingName
-	 * @param	object settingPayload
-	 * @param	bool [optional] reRenderUI
-	 * @param	bool [optional] fromSocket
+	 * @param	object payload
 	 * @return	void
 	 */
 	static async handleUpdate(payload)
 	{
-		// To be the "update GM" the user must be both a GM and the
-		// "active" GM with the lowest user ID
-		const isUpdateGM = game.user.isGM && !(game.users.filter(user => user.isGM && user.active).some(other => other._id < game.user._id));
-		if (payload.showSelect && !isUpdateGM)
-		{
-			BracketInitiative.supplyInitiativeRoll();
-		}
-		else if (payload.token && payload.explicitRoll)
-		{
-			if (isUpdateGM)
-			{
-				// Set the action flags
-				const combatant = game.combats.active.combatants.find((c) => c.token.id == payload.token);
-				if (!combatant)
-				{
-					ui.notifications.warn(`Tried to update token ${payload.token}, but it wasn't in the combatant list`);
-					return;
-				}
-				if (payload.explicitRoll)
-					await combatant.setFlag(BracketInitiative.MODULE_NAME, BracketInitiative.FLAG_ROLL_RESULT, payload.explicitRoll);
-			}
-			else
-			{
-				// Sanity check this user is allowed to do this
-				const affectableTokens = BracketInitiative.getLegalTokens();
-				if (!(affectableTokens.find((token) => token.id == payload.token)))
-					return;
+		const self = BracketInitiative;
 
-				console.log("BracketInitiative | Emitting socket event with payload", payload);
-				game.socket.emit(BracketInitiative.SOCKET_NAME, payload);
-			}
-		}
-	}
-
-	/**
-	 * Assuming conditions permit, display the dialog to supply a manual
-	 * initiative roll for the selected token(s).
-	 *
-	 * @param {string} combatantId  Optional ID of a combatant to choose an action for
-	 * @return	bool
-	 */
-	static supplyInitiativeRoll(combatantId)
-	{
-		// Make sure an encounter exists!
-		if (!game.combat)
-		{
-			ui.notifications.error('Cannot choose a round action outside of combat!');
-			return false;
-		}
-
-		// Determine what tokens we can affect
-		const affectTokens = BracketInitiative.getTokenListForDialog(combatantId);
-		if (!affectTokens)
-			return false;
-
-		(async function(affectTokens)
-		{
-			for (let i = 0; i < affectTokens.length; i++)
-			{
-				if (!BracketInitiative.canTokenRollInitiative(affectTokens[i]))
-				{
-					ui.notifications.warn("Only the GM can modify initiative once it has been rolled for the round");
-					continue;
-				}
-				/*
-				const dialogData = {
-				};
-				const dialogContent = await renderTemplate(
-					"modules/bracket-initiative/templates/initiative.html",
-					dialogData
-				);
-				const name = affectTokens[i].name;
-				new Dialog({
-					title: `Supply Manual Initiative: ${name}`,
-					content: dialogContent,
-					buttons: {
-						yes:{
-							icon: '<i class="fas fa-dice-d20"></i>',
-							label: 'Set Action',
-							callback: (html) => {
-								BracketInitiative.setAction(affectTokens[i], html);
-							}
-						},
-						no:{
-							icon: '<i class="fas fa-times-circle"></i>',
-							label: 'Cancel'
-						}
-					}
-				}).render(true);
-				*/
-				// console.log(affectTokens.combatant.rollInitiativeDialog();
-			}
-		})(affectTokens);
-		return true;
-	}
-
-	/**
-	 * Determine what set of tokens should be operated on for
-	 * supplyInitiativeRoll()
-	 *
-	 * @param {string} combatantId	Optional specific combatant
-	 * @return {mixed}	Array of tokens on success, false on error
-	 */
-	static getTokenListForDialog(combatantId)
-	{
-		const activeCombatants = game.combats.active.combatants;
-		let affectTokens = [];
-		if (combatantId)
-		{
-			// Find token corresponding to combatant
-			const combatant = activeCombatants.find(ac => ac.id == combatantId);
-			const affectableTokens = BracketInitiative.getLegalTokens();
-			affectTokens = affectableTokens.filter(at => at.id == combatant.tokenId);
-			if (affectTokens.length == 0)
-			{
-				if (game.user.isGM)
-					ui.notifications.error("Token for that combatant was not found");
-				else
-					ui.notifications.error("You don't own that combatant's token");
-				return false;
-			}
-		}
-		else if (game?.canvas?.tokens?.controlled?.length > 0)
-		{
-			// Filter the list down to just active combatants
-			const controlledTokens = game.canvas.tokens.controlled;
-			const affectableTokens = BracketInitiative.getLegalTokens();
-			affectTokens = activeCombatants.filter(ac =>
-				controlledTokens.find((ct) => ct.id == ac.token.id) &&
-				affectableTokens.find((aft) => aft.id == ac.token.id)
-			)?.map(ac => ac.token);
-			if (affectTokens.length == 0)
-			{
-				ui.notifications.error("None of the tokens selected can be affected. Try deselecting, or selecting different tokens");
-				return false;
-			}
-		}
-		else
-		{
-			const ownedTokens = canvas.tokens.ownedTokens;
-			affectTokens = activeCombatants.filter(ac => ownedTokens.find((ot) => ot.id == ac.token.id))?.map(ac => ac.token);
-
-			// If we get here, something went screwy; let the user know
-			if (affectTokens.length == 0)
-			{
-				ui.notifications.error("No tokens found that could be affected");
-				return false;
-			}
-		}
-		return affectTokens;
-	}
-
-	/**
-	 * Determine what tokens the current user can legally affect
-	 *
-	 * @return	{array}
-	 */
-	static getLegalTokens()
-	{
-		const controlledTokens = canvas.tokens.controlled;
-		const ownedTokens = canvas.tokens.ownedTokens;
-		let affectableTokens = controlledTokens.concat(ownedTokens);
-		affectableTokens = [...new Map(affectableTokens.map((m) => [m.id, m])).values()];
-		return affectableTokens;
-	}
-
-	/**
-	 * Determine if the token's in a state where it's legal for it to
-	 * choose its next action
-	 *
-	 * @param	{Token} token
-	 * @return	bool
-	 */
-	static canTokenRollInitiative(token)
-	{
-		if (game.user.isGM)
-			return true;
-		// Initiative has already been rolled for all active combatants?
-		const combatants = game.combats.active.combatants;
-		let allRolled = true;
-		for (let c of combatants)
-		{
-			// Only truly dead combatants are ignored
-			const isDead = BracketInitiative.isDead(c.token)
-			if (!isDead && (c.initiative == null || typeof c.initiative == 'undefined'))
-			{
-				allRolled = false;
-				break;
-			}
-		}
-		return !allRolled;
-	}
-
-	/**
-	 * Invoked by the Choose Round Action dialog when an action is chosen
-	 *
-	 * @param	token
-	 * @param	html
-	 * @return	void
-	 */
-	static async setAction(token, html)
-	{
-		const formData = (new FormDataExtended(html[0].querySelector('form.speedFactorInitiative'))).object;
-		if (!(formData['init-mod-action'] in BracketInitiative.actionModifiers) || !(formData['init-mod-bonus-action'] in BracketInitiative.actionModifiers))
-		{
-			ui.notifications.error("Invalid action selection");
-			BracketInitiative.supplyInitiativeRoll(token.combatant.id);
+		// Only the GM handles socket updates
+		if (!self.isUpdateGM())
 			return;
-		}
-		if (formData['init-roll'] && (!(Number.isNumeric(formData['init-roll'])) || !(Number.isInteger(Number.fromString(formData['init-roll'])))))
+
+		if (payload?.type === 'initiative')
 		{
-			ui.notifications.error("Initiative roll must be a valid integer");
-			BracketInitiative.supplyInitiativeRoll(token.combatant.id);
-			return;
+			self.debug(payload);
+			if (payload?.combatant)
+				self.checkForFollowUpdates(payload.combatant);
 		}
-		const payload = {
-			token: token.id,
-			actionChosen: formData['init-mod-action'],
-			actionTarget: formData['action-target'],
-			bonusActionChosen: formData['init-mod-bonus-action'],
-			bonusActionTarget: formData['bonus-action-target'],
-			explicitRoll: formData['init-roll']
-		};
-		BracketInitiative.handleUpdate(payload);
 	}
 
 	/**
@@ -263,141 +51,49 @@ export class BracketInitiative
 	 */
 	static async handleCombatEvent(eventType, combat, round, time)
 	{
-		console.log("BracketInitiative | event type", eventType, "combat", combat, "round", round, "time", time);
+		const self = BracketInitiative;
+
+		if (!self.isUpdateGM())
+			return;
+
 		// At the top of a new round, clear all initiative rolls
 		if (eventType == "round")
 		{
-			console.log("Reset all");
 			await combat.resetAll();
 		}
-		/*
-		// Then display a prompt to each player to select an action
-		if ((eventType == "start" || eventType == "round") && game.user.isGM)
-		{
-			game.socket.emit(BracketInitiative.SOCKET_NAME, {'showSelect': true});
-		}
-		*/
-		// If turn advancement occurred, do a sanity check to ensure all combatants chose an action
-		/*
-		if (eventType == "turn")
-		{
-			if (!BracketInitiative.checkActionsChosen(combat, round))
-				return;
-			if (!BracketInitiative.checkInitiativeRolled(combat, round))
-				return;
-		}
-		*/
 	}
 
 	/**
-	 * When a turn is advanced, check to ensure all combatants have a
-	 * chosen action and abort advancement if not.
+	 * Handle a combatant-related update event that we care about
 	 *
-	 * @return	{bool}
+	 * @param	Combatant combatant
+	 * @param	object update
+	 * @return	void
 	 */
-	static checkActionsChosen(combat, round)
+	static async handleCombatantEvent(combatant, update)
 	{
-		// TODO: This is probably not necessary
-		// No combatants = nothing to do
-		if (combat.combatants?.size == 0)
+		const self = BracketInitiative;
+		self.debug("Handling Combatant Event");
+		// If the update didn't pertain to initiative, we don't care
+		if (!update?.hasOwnProperty('initiative'))
 		{
-			return true;
+			self.debug("Non-initiative update; we don't care", update);
+			return;
 		}
-		for (let combatant of combat.combatants)
-		{
-			const isDead = BracketInitiative.isDead(combatant.token);
-			if (!(isDead) && (!combatant.getFlag(BracketInitiative.MODULE_NAME, BracketInitiative.FLAG_ACTION_CHOSEN) || !combatant.getFlag(BracketInitiative.MODULE_NAME, BracketInitiative.FLAG_BONUS_ACTION_CHOSEN)))
-			{
-				if (combatant.isNPC == false || game.user.isGM) // TODO: Should check player-is-owner, not isNPC; wrap into function
-				{
-					ui.notifications.warn(`Combatant ${combatant.name} must choose actions!!`);
-				}
-				else
-				{
-					ui.notifications.warn("A combatant still needs to choose actions!");
-				}
-				if (game.user.isGM)
-				{
-					combat.update({'turn': 0});
-					round.turn = 0;
-				}
-				return false;
-			}
-		}
-		return true;
-	}
 
-	/**
-	 * When a turn is advanced, check to ensure all combatants have rolled
-	 * initiative and abort advancement if not.
-	 *
-	 * @return	{bool}
-	 */
-	static checkInitiativeRolled(combat, round)
-	{
-		// TODO: Probably don't need this anymore
-		// No combatants = nothing to do
-		if (combat.combatants?.size == 0)
+		// If the update was to clear out initiative, don't fight with it
+		if (update.intiative == 'undefined' || update.initiative == null)
 		{
-			return true;
+			self.debug("Initiative update was a reset; leaving alone");
+			return;
 		}
-		for (let combatant of combat.combatants)
-		{
-			const isDead = BracketInitiative.isDead(combatant.token);
-			if (!(isDead) && (typeof combatant.initiative == 'undefined' || combatant.initiative == null))
-			{
-				if (combatant.isNPC == false || game.user.isGM) // TODO: Should check player-is-owner, not isNPC; wrap into function
-				{
-					ui.notifications.warn(`Combatant ${combatant.name} must roll initiative!`);
-				}
-				else
-				{
-					ui.notifications.warn("A combatant still needs to roll initiative!");
-				}
-				if (game.user.isGM)
-				{
-					combat.update({'turn': 0});
-					round.turn = 0;
-				}
-				return false;
-			}
-		}
-		return true;
-	}
+		self.debug(update);
 
-	/**
-	 * Check that an imminent updateCombatant call can be made
-	 *
-	 * @param {object} combatant
-	 * @param {object} update
-	 * @params {object} args
-	 * @params {string} userId
-	 * @retur	bool
-	 */
-	static validateCanUpdate(combatant, update, args, userId)
-	{
-		if (update.initiative && !game.users.get(userId).isGM)
-		{
-			combatant.initiative = null;
-			update.initiative = null;
-			update._id = undefined;
-			args.diff = false;
-			args.render = false;
-			ui.notifications.error("You cannot affect initiative manually");
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * Determine if the indicated token is dead
-	 *
-	 * @param	{Token}
-	 * @return	{bool}
-	 */
-	static isDead(token)
-	{
-		return token?.actorData?.effects?.find((e) => e.label == 'Dead');
+		self.handleUpdate({
+			type: 'initiative',
+			combatant: combatant?.id
+		});
+		return;
 	}
 
 	/**
@@ -406,11 +102,14 @@ export class BracketInitiative
 	 *
 	 * @return	void
 	 */
-	static async rollInitiativeCallback()
+	static async rollAllInitiativeCallback()
 	{
-		if (!game.user.isGM || !game.combats.active)
+		self = BracketInitiative;
+		if (!self.isUpdateGM() || !game.combats.active)
 			return;
 
+		// Wait until all the initiative rolls are finished coming in, then
+		// reset the turn order to 0
 		await (async function() {
 			const poll = resolve => {
 				if (game.combats.active.combatants.filter(c => (typeof c.initiative == "undefined" || c.initiative == null)).length == 0)
@@ -423,24 +122,110 @@ export class BracketInitiative
 		})();
 		game.combats.active.update({'turn': 0});
 
-		// Scan combatants for followers
+		// Scan combatants for followers and update accordingly
 		for (let combatant of game.combats.active.combatants)
 		{
-			const followedName = combatant?.token?.actor?.flags['bracket-initiative']?.follow;
-			if (!followedName)
-			{
-				continue;
-			}
+			self.updateFollowers(combatant.id);
+		}
+	}
+
+	/**
+	 * Upon rolling individual initiative, if the combatant is a follower,
+	 * follow its parent, if able. Alternately, if the combatant *has*
+	 * followers, update them.
+	 *
+	 * @param   {string}combatantId
+	 * @return	bool	Whether or not we updated the combatant
+	 */
+	static async checkForFollowUpdates(combatantId)
+	{
+		const self = BracketInitiative;
+		if (!game.combats.active || !combatantId)
+		{
+			self.debug("No combats active, or combatantId not supplied");
+			self.debug(combatantId);
+			return false;
+		}
+		const combatant = game.combats.active.combatants.get(combatantId);
+		if (!combatant)
+		{
+			self.debug("Combatant not found");
+			return false;
+		}
+
+		let followedInitiative = await self.followLeader(combatant);
+		self.leadFollowers(combatant);
+		return followedInitiative;
+	}
+
+	/**
+	 * Do we follow a leader?
+	 *
+	 * @param	Combatant combatant
+	 * @return	bool	Whether or not we followed
+	 */
+	static async followLeader(combatant)
+	{
+		const self = BracketInitiative;
+		// Is this combatant a follower?
+		const followedName = combatant?.actor?.getFlag(self.MODULE_NAME, 'follow');
+		if (followedName)
+		{
+			// Is their followed character in this combat?
 			const followed = game.combats.active.combatants.filter(c => c.name == followedName);
 			if (!followed || followed.length == 0)
 			{
-				continue;
+				self.debug("Followed combatant", followedName, "is not in this combat");
+				return false;
 			}
-			if (typeof followed[0].initiative != "undefined")
+			else if (Number(followed[0].initiative) != "NaN" && followed[0].initiative != null)
 			{
-				combatant.update({'initiative': followed[0].initiative});
+				self.debug("Overriding initiative with that of followed combatant");
+				await combatant.update({'initiative': followed[0].initiative});
+				return true;
+			}
+			self.debug("Followed combatant", followedName, "doesn't yet have a valid initiative");
+			return false;
+		}
+		self.debug("Not following anyone");
+		return false;
+	}
+
+	/**
+	 * Do we lead followers?
+	 *
+	 * @param	Combatant combatant
+	 * @return	bool	Whether or not we were the leader
+	 */
+	static leadFollowers(combatant)
+	{
+		const self = BracketInitiative;
+		// Does this combatant have any followers?
+		const followers = game.combats.active.combatants.filter((c) => c.actor?.getFlag(self.MODULE_NAME, 'follow') || false);
+
+		// Are any of them followers of this combatant?
+		if (followers.length > 0)
+		{
+			for (const f of followers)
+			{
+				if (f.actor?.getFlag(self.MODULE_NAME, 'follow') === combatant.actor.name)
+					self.checkForFollowUpdates(f.id);
 			}
 		}
+		self.debug("No followers");
+	}
+
+	/**
+	 * Determine whether or not the current user is the "update GM"
+	 *
+	 * To be the "update GM" the user must be both a GM and the
+	 * "active" GM with the lowest user ID
+	 *
+	 * @return	bool
+	 */
+	static isUpdateGM()
+	{
+		return game.user.isGM && !(game.users.filter(user => user.isGM && user.active).some(other => other._id < game.user._id));
 	}
 
 	/**
@@ -505,28 +290,5 @@ export class BracketInitiative
 			}
 		}
 		return roll;
-	}
-
-	/**
-	 * Patch the combatant sorting so that NPCs are always sorted after
-	 * PCs and we otherwise sort alphabetically by combatant name.
-	 *
-	 * @param
-	 * @param
-	 * @return
-	 */
-	static wrappedSortCombatants(a, b)
-	{
-		const ia = Number.isNumeric(a.initiative) ? a.initiative : -Infinity;
-		const ib = Number.isNumeric(b.initiative) ? b.initiative : -Infinity;
-		if ((ib - ia) == 0)
-		{
-			if (a.isNPC && !b.isNPC)
-				return 1;
-			if (!a.isNPC && b.isNPC)
-				return -1;
-			return (a.name > b.name ? 1 : -1);
-		}
-		return (ib - ia);
 	}
 }
--- esmodules/bracket.js
+++ esmodules/bracket.js
@@ -2,38 +2,31 @@ import {
 	BracketInitiative
 } from './bracket.class.js';
 
-Handlebars.registerHelper('json', function (obj)
-{
-	return JSON.stringify(obj);
-});
-Handlebars.registerHelper('numEq', function (a, b)
-{
-	return (Number.fromString(a) === Number.fromString(b));
-});
-
 /**
  * On initialization, patch the actor document and the Combatant
  * prototype's initiative formula.
  */
 Hooks.once("init", function() {
+	// Hook our rollAll callback
 	libWrapper.register(BracketInitiative.MODULE_NAME, 'Combat.prototype.rollAll', function(wrapped, ...args) {
 		const result = wrapped(...args);
-		BracketInitiative.rollInitiativeCallback();
+		BracketInitiative.rollAllInitiativeCallback();
 		return result;
 	}, 'WRAPPER');
 	libWrapper.register(BracketInitiative.MODULE_NAME, 'Combat.prototype.rollNPC', function(wrapped, ...args) {
 		const result = wrapped(...args);
-		BracketInitiative.rollInitiativeCallback();
+		BracketInitiative.rollAllInitiativeCallback();
 		return result;
 	}, 'WRAPPER');
+
+	// Hook the D20Roll modifier
 	libWrapper.register(BracketInitiative.MODULE_NAME, 'CONFIG.Dice.D20Roll.prototype._onDialogSubmit', function(wrapped, html, advantageMode) {
 		const roll = wrapped(html, advantageMode);
 		return BracketInitiative.patchD20RollOnDialogSubmit(roll, html);
 	}, 'WRAPPER');
-	libWrapper.register(BracketInitiative.MODULE_NAME, 'Combat.prototype._sortCombatants', function(wrapped, ...args) {
-		const originalResult = wrapped(...args); // Then just discard it.
-		return BracketInitiative.wrappedSortCombatants(...args);
-	}, 'WRAPPER');
+
+	// Setup our debug property
+	CONFIG.debug.BracketInitiative = false;
 });
 
 /**
@@ -42,12 +35,11 @@ Hooks.once("init", function() {
  */
 Hooks.once("ready", function() {
 	if (!game.modules.get('lib-wrapper')?.active && game.user.isGM)
-	{
 		ui.notifications.error("Module BracketInitiative requires the 'libWrapper' module. Please install and activate it.");
-	}
+
 	if (game.combats.active)
 	{
-		console.log("BracketInitiative | Active combat detected; activating BracketInitiative socket");
+		BracketInitiative.debug("Active combat detected; activating BracketInitiative socket");
 		game.socket.on(BracketInitiative.SOCKET_NAME, BracketInitiative.handleUpdate);
 	}
 });
@@ -56,7 +48,7 @@ Hooks.once("ready", function() {
  * When a combat is created, activate our socket!
  */
 Hooks.on("createCombat", () => {
-	console.log("BracketInitiative | New combat detected; activating BracketInitiative socket");
+	BracketInitiative.debug("New combat detected; activating BracketInitiative socket");
 	game.socket.on(BracketInitiative.SOCKET_NAME, BracketInitiative.handleUpdate);
 });
 
@@ -64,16 +56,8 @@ Hooks.on("createCombat", () => {
  * Turn our socket off when combat ends.
  */
 Hooks.on("deleteCombat", () => {
-	console.log("BracketInitiative | Dectivating BracketInitiative socket");
+	BracketInitiative.debug("Dectivating BracketInitiative socket");
 	game.socket.off(BracketInitiative.SOCKET_NAME);
-});
-
-/**
- * When a user executes the Choose Round Action macro, fire off an event
- * so the class handles it
- */
-Hooks.on('BracketInitiative_supplyInitiativeRoll', () => {
-	return BracketInitiative.chooseRoundAction();
 });
 
 /**
@@ -89,16 +73,10 @@ Hooks.on("combatTurn", (combat, round, time) => {
 Hooks.on("combatRound", (combat, round, time) => {
 	BracketInitiative.handleCombatEvent("round", combat, round, time);
 });
+Hooks.on("updateCombatant", (combatant, update, diff, id) => {
+	BracketInitiative.handleCombatantEvent(combatant, update);
+});
 
 Hooks.on("renderDialog", (dialog, $html, appData) => {
 	BracketInitiative.enhanceInitiativeDialog(dialog, $html, appData);
 });
-/*
-Hooks.on("preUpdateCombatant", (combatant, update, args, userId) => {
-	return BracketInitiative.validateCanUpdate(combatant, update, args, userId);
-});
-
-Hooks.on("updateCombatant", (combatant, update, args, userId) => {
-	return BracketInitiative.validateCanUpdate(combatant, update, args, userId);
-});
-*/
